10bf483ca7c50227dde11d579dd8018fd39a8698
Alex You <alexyou8021@gmail.com>
2019-07-26 18:20:10 -0700

    request

--- main.go
+++ main.go
@@ -8,20 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
-type Info struct {
-	attachments []map[string]string
-	avatar_url string
-	created_at int
-	group_id string
-	id string
-	name string
-	sender_id string
-	sender_type string
-	source_guid string
-	system bool
-	text string
-	user_id string
-}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -38,9 +24,7 @@ func main() {
 		c.String(http.StatusOK, "success")
 	})
 	router.POST("/" , func(c *gin.Context) {
-		var data Info
-        	c.BindJSON(&data)
-		log.Println(data.name)
+		log.Println(c.Request)
 	})
 
 	router.Run(":" + port)
