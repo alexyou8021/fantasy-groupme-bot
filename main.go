0397f365045bd97d57d378a344aab191fff16bfb
Alex You <alexyou8021@gmail.com>
2019-07-28 18:40:30 -0700

    parse

--- main.go
+++ main.go
@@ -10,6 +10,21 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+type msg struct {
+	attachments map[string]string
+	avatar_url string
+	created_at int
+	group_id string
+	id string
+	name string
+	sender_id string
+	sender_type string
+	source_guid string
+	system bool
+	text string
+	user_id string
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -22,13 +37,17 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		x, _ := ioutil.ReadAll(c.Request.Body)
-        	log.Printf("%s", string(x))
 		c.String(http.StatusOK, "success")
 	})
 	router.POST("/", func(c *gin.Context) {
 		x, _ := ioutil.ReadAll(c.Request.Body)
+		message := new(msg)
+		err := c.Bind(message)
+		if err != nil {
+			log.Println(err)
+		}
         	log.Printf("%s", string(x))
+        	log.Printf("%s", message.text)
 		c.JSON(http.StatusOK, c)
 	})
 
