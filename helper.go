bc96ba6d51c966c869ceb8e142afa7d3655ad36b
Alex You <alexyou8021@gmail.com>
2019-08-02 22:17:39 -0700

    query sleeper players

--- helper.go
+++ helper.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"os"
+	"io/ioutil"
+	"encoding/json"
+	"net/http"
 
 	"database/sql"
 	_ "github.com/lib/pq"
@@ -13,20 +16,28 @@ func createPlayersTable() {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-                result, err := db.Exec("DROP TABLE players;")
-                result, err = db.Exec("CREATE TABLE players (id int, name varchar(255), position varchar(255));")
+		result, err := db.Exec("DROP TABLE players;")
+		result, err = db.Exec("CREATE TABLE players (id int, name varchar(255), position varchar(255));")
 		log.Println(result)
 		log.Println(err)
 	}
 }
 
 func storePlayers() {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	} else {
-                result, err := db.Exec("CREATE TABLE players (id varchar(255), name varchar(255))")
-		log.Println(result)
-		log.Println(err)
-	}
+	//db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	//if err != nil {
+	//	log.Fatal(err)
+	//} else {
+	//        result, err := db.Exec("CREATE TABLE players (id varchar(255), name varchar(255))")
+	//	log.Println(result)
+	//	log.Println(err)
+	//}
+	url := "https://api.sleeper.app/v1/players/nfl/"
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	var players []map[string]interface{}
+	json.Unmarshal(bodyBytes, &players)
+	log.Println(players)
+
 }
